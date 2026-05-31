bd67b5defd40aa6ae9e615219f08c1c97b283649
NguyenMinhThaiSon_25020351 <25020351@vnu.edu.vn>
2026-05-31 16:20:25 +0700

    Fix: Add GsonProvider utility to handle LocalDateTime serialization

---
+++ common/src/main/java/com/auction/utils/GsonProvider.java
@@ -0,0 +1,22 @@
+package com.auction.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSerializer;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonPrimitive;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class GsonProvider {
+    private static final Gson gson = new GsonBuilder()
+            .registerTypeAdapter(LocalDateTime.class, (JsonSerializer<LocalDateTime>) (src, typeOfSrc, context) ->
+                    new JsonPrimitive(src.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)))
+            .registerTypeAdapter(LocalDateTime.class, (JsonDeserializer<LocalDateTime>) (json, typeOfT, context) ->
+                    LocalDateTime.parse(json.getAsString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+            .create();
+
+    public static Gson getGson() {
+        return gson;
+    }
+}
